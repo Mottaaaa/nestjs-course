02f4c97f07f4e4dac6012ff5b89708eefe63c77a
Pedro Mota <pedro10mota@msn.com>
2022-11-23 06:09:11 +0000

    78. Restricting Deleting A Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -37,12 +37,14 @@ export class TasksService {
     return task;
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete({ id });
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
 
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
+
+    return;
   }
 
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
